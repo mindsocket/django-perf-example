ea8f572dacb3c7bd9c4ffc73e49947c057d5dffa
Roger Barnes <roger@mindsocket.com.au>
2013-05-02 00:19:16 +1000

    Added basic content test

--- perf_example/views.py
+++ perf_example/views.py
@@ -2,7 +2,7 @@ import time
 from django.shortcuts import render_to_response, stream_to_response
 from django.views.generic.base import StreamingTemplateView
 
-def stream(count=50):
+def stream(count=30):
     """Slow generator"""
     for _ in xrange(count):
         yield "Polo! " * 100
@@ -18,7 +18,7 @@ def broken_stream(count=20):
         yield ("%d " % _) * 100
         time.sleep(0.1)
 
-# standard view, 1 second in-view delay, 5 seconds of streaming data
+# standard view, 1 second in-view delay, 3 seconds of streaming data
 def perftest(request, func=render_to_response, stream_func=stream, template="index.html"):
     # Simulate some slow work
     time.sleep(1)
@@ -27,7 +27,8 @@ def perftest(request, func=render_to_response, stream_func=stream, template="ind
 
 # stream_to_response version - still affected by initial 1 second delay
 def streaming_perftest(request, template="index.html", **kwargs):
-    return perftest(request, func=stream_to_response, template=template, **kwargs)
+    result = perftest(request, func=stream_to_response, template=template, **kwargs)
+    return result
 
 # eager streamed version, only streams template body as middleware has already streamed header
 def eager_streaming_perftest(request):
